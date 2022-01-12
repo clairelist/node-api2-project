04b37b7c02209fd7835e7fc89912847f5d0e450b
Claire List <claire.list2501@gmail.com>
2022-01-11 22:28:17 -0700

    put endpoint working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // LOGIC SECTIONR
 
 //// GET ALL POSTS
-router.get('/',(req,res)=>{
+router.get('/', (req,res) => {
     Post.find(req.query)
     .then(posts=>{
      res.status(200).json(posts);
@@ -17,10 +17,10 @@ router.get('/',(req,res)=>{
 });
 
 //// GET SINGULAR POST -- by id
-router.get('/:id',(req,res)=>{
+router.get('/:id', (req,res) => {
     Post.findById(req.params.id)
     .then(post=>{
-        if(post){
+        if (post) {
             res.status(200).json(post);
         } else {
             res.status(404).json({message: 'The post with the specified ID does not exist'});
@@ -32,25 +32,52 @@ router.get('/:id',(req,res)=>{
 });
 
 //// POST POST PASSES THE CORRECT INFO IN THE REQ BODY AND THEN RETURNS THE RIGHT OBJECT
-router.post('/',(req,res)=>{
+router.post('/', (req,res) => {
     const {title,contents} = req.body;
     //this data must come before we create the object !
    
-    if(!title || !contents){
-        res.status(400).json({message: 'Please provide title and contents for the post'})
+    if (!title || !contents) {
+        res.status(400).json({message: 'Please provide title and contents for the post'});
     } else {
         Post.insert({title,contents})
         .then(({id})=>{ 
             return Post.findById(id); //we nest this here because the returned object of post with this id is expected
         })
         .then(post=>{
-            res.status(201).json(post)
+            res.status(201).json(post); ///...and now HERE we actually post it to the server lol
         }).catch(err=>{
             console.error(err);
             res.status(500).json({ message: "There was an error while saving the post to the database" })
         })
     }
+});
+
+router.put('/:id', (req,res) => {
+    const {title,contents} = req.body;
+    //this data must come before we create the object !
+   
+    if (!title || !contents) {
+        res.status(400).json({message: 'Please provide title and contents for the post'});
+    } else {
+        Post.findById(req.params.id)
+        .then(post=>{
+            if (!post) {
+             res.status(404).json({message: 'The post with the specified ID does not exist'});
+            } else {
+               return Post.update(req.params.id,req.body)
+            }
+        }).then(info=>{
+            if (info) {
+                return Post.findById(req.params.id);
+            } 
+        }).then(post=>{
+            res.status(200).json(post);
+        }).catch(err=>{
+            res.status(500).json({ message: "The post information could not be modified", error:err });
+        })
+    }
 })
+// ...basically I had it a bit backwards, LOL !
 
 //RETURN -- PSUED EXPORT SECTIONR
 module.exports = router;
