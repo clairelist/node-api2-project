3ee882ce8d39837e80e29e4edd0c47a0023b3164
Claire List <claire.list2501@gmail.com>
2022-01-11 17:29:39 -0700

    get by id built

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,6 +4,8 @@ const Post = require('./posts-model.js');
 const router = express.Router();
 
 // LOGIC SECTIONR
+
+//// GET ALL POSTS
 router.get('/',(req,res)=>{
     Post.find(req.query)
     .then(posts=>{
@@ -14,4 +16,20 @@ router.get('/',(req,res)=>{
     })
 });
 
+//// GET SINGULAR POST -- by id
+router.get('/:id'),(req,res)=>{
+    Post.findById(req.params.id)
+    .then(post=>{
+        if(post){
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({message: 'The post with the specified ID does not exist'});
+        }
+    }).catch(err=>{
+        console.error(err);
+        res.status(500).json({message: 'The information could not be retrived'});
+    })
+}
+
 //RETURN -- PSUED EXPORT SECTIONR
+module.exports = router;
