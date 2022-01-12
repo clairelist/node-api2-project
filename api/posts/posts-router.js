b264c37b7a537e108a5fa4fea7051dac85d7244e
Claire List <claire.list2501@gmail.com>
2022-01-11 17:17:56 -0700

    all posts built

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,17 @@
-// implement your posts router here
+// DATA SECTIONR
+const express = require('express');
+const Post = require('./posts-model.js');
+const router = express.Router();
+
+// LOGIC SECTIONR
+router.get('/',(req,res)=>{
+    Post.find(req.query)
+    .then(posts=>{
+     res.status(200).json(posts);
+    }).catch(err=>{
+        console.error(err);
+        res.status(500).json({error:`The posts information could not be retrieved`})
+    })
+});
+
+//RETURN -- PSUED EXPORT SECTIONR
